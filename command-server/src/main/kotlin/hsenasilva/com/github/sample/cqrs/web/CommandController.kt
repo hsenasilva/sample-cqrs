8ece8281aa57fcdd51622952fb035d27d1bdde74
Henrique Sena da Silva <hsena91@gmail.com>
2019-08-01 00:02:25 -0300

    adding requestbody annotation

--- command-server/src/main/kotlin/hsenasilva/com/github/sample/cqrs/web/CommandController.kt
+++ command-server/src/main/kotlin/hsenasilva/com/github/sample/cqrs/web/CommandController.kt
@@ -5,10 +5,7 @@ import hsenasilva.com.github.sample.cqrs.domain.ListSampleParameter
 import hsenasilva.com.github.sample.cqrs.domain.SampleParameter
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.CompletableFuture
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
@@ -22,7 +19,7 @@ class CommandController(private val commandGateway: CommandGateway) {
 
     @PostMapping(value = ["/samples"])
     @ResponseStatus(value = HttpStatus.CREATED)
-    fun getPositions(@Valid @NotNull parameter: SampleParameter): CompletableFuture<Unit> =
+    fun getPositions(@RequestBody @Valid @NotNull parameter: SampleParameter): CompletableFuture<Unit> =
             this.commandGateway.send(ListSampleCommand(listSampleParameter = ListSampleParameter(parameter.id, parameter.stuff)))
 
 }
